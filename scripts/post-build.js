e61d741f4f6a2815134e5a623926c7441a4f027c
Serhii Mamedov <bboyvivus@gmail.com>
2021-02-25 22:31:40 +0200

    Replace single quote with double quote

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -1,5 +1,5 @@
 const { resolve } = require("path");
 const { addExtToFilesRecursive } = require("../lib/cjs");
 
-addExtToFilesRecursive(resolve(__dirname, "..", "lib", "esm"), 'js');
+addExtToFilesRecursive(resolve(__dirname, "..", "lib", "esm"), "js");
 console.log("Added js extensions to esm build files");
